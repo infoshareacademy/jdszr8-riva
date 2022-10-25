36becaebdb53ba6836ba5f5a48aec03d79f1fdcf
Aron Mozejko <aron.mozejko@gmail.com>
2022-10-25 19:08:23 +0200

    small changes and file reorganisation. update of main file

--- RESTAURANTS_PYTHON/bangalore_data_cleaning/data_cleaning/cleaning_functions/name_column.py
+++ RESTAURANTS_PYTHON/bangalore_data_cleaning/data_cleaning/cleaning_functions/name_column.py
@@ -1,12 +1,13 @@
-def names_cleaning(bangalore_data):
-    """This function clean name column. It strips phone
+def clean_name_column(df):
+    """The function takes dataframe as an argument and return dataframe.
+    This function clean name column. It strips phone
     It uses regex pattern"""
 
-    names = bangalore_data['name'].replace(
+    names = df['name'].replace(
         ['Ã.*©', '.*©', 'Ã.*¨', 'Ã.*±', 'Ã.*¢', 'Ã.*°',
             'Ã.*»', 'Ã.*ª', 'Ã.*'],
         ['é', 'é', 'é', 'ñ', 'â', '°', 'û', 'e', '’'],
         regex=True)
-    bangalore_data.update(names)
+    df.update(names)
 
-    return bangalore_data
+    return df
